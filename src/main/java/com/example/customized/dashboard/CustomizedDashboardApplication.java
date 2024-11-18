11fd0d34d303714e7f8078b9d22ce705c44593ed
Aria <yliang88705@student.sfbu.edu>
2024-11-17 21:57:35 -0800

    initial commit from spring initializer

---
+++ src/main/java/com/example/customized/dashboard/CustomizedDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.customized.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomizedDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomizedDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customized/dashboard/CustomizedDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customized.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomizedDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
